d3cc6bd7fc67091664cfb9b20b2f5e0453cfae1d
Kamil Moskała <91079590+moskalakamil@users.noreply.github.com>
2025-03-12 13:44:50 +0100

    docs: ga+cookies (#3)
    * docs: add ga&cookies
    
    * refactor: delete package-lock.json

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -32,6 +32,21 @@ export default {
         type="image/png"
         href="https://docs.thewidlarzgroup.com/react-native-video/favicon.png"
       />
+      {/* Google Tag Manager*/}
+      <script>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P4K9VDMV');`}
+      </script>
+      {/*End Google Tag Manager*/}
+      <script
+        id="Cookiebot"
+        src="https://consent.cookiebot.com/uc.js"
+        data-cbid="c405153a-f05d-48df-bec6-a822fef9e880"
+        data-blockingmode="auto"
+        type="text/javascript"></script>
     </>
   ),
   logo: (
